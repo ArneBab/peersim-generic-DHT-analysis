e5aa8413f6eb478947bd97ca41b55867a1dca640
baumeist <baumeist@adobe.com>
2017-12-03 00:11:46 -0800

    Test static topology generation

---
+++ analysis/test/test_topology.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+'''
+Updated on September, 2017
+@author: Todd Baumeister <tbaumeist@gmail.com>
+
+Unit test for routing tree
+'''
+import unittest
+
+from lib.topology_generator import TopologyGenerator
+
+
+class TestTopology(unittest.TestCase):
+    '''
+    Test the Topology Generator Class
+    '''
+
+    def test_structured(self):
+        graph = TopologyGenerator.generate_structured_topology(10, 3)
+        self.assertTrue(graph.number_of_nodes() == 10)
+        self.assertTrue(len(graph.edge[0]) == 3)
+        self.assertTrue(graph.graph['invariant'] == 2)
+
+        graph = TopologyGenerator.generate_structured_topology(10, 4)
+        self.assertTrue(graph.number_of_nodes() == 10)
+        self.assertTrue(len(graph.edge[0]) == 5)
+        self.assertTrue(graph.graph['invariant'] == 4)
+
+        graph = TopologyGenerator.generate_structured_topology(10, 5)
+        self.assertTrue(graph.number_of_nodes() == 10)
+        self.assertTrue(len(graph.edge[0]) == 5)
+        self.assertTrue(graph.graph['invariant'] == 4)
+
+        graph = TopologyGenerator.generate_structured_topology(10, 6)
+        self.assertTrue(graph.number_of_nodes() == 10)
+        self.assertTrue(len(graph.edge[0]) == 7)
+        self.assertTrue(graph.graph['invariant'] == 8)
+
+
+        graph = TopologyGenerator.generate_structured_topology(1000, 6)
+        self.assertTrue(graph.number_of_nodes() == 1000)
+        self.assertTrue(len(graph.edge[0]) == 7)
+        self.assertTrue(graph.graph['invariant'] == 8)
