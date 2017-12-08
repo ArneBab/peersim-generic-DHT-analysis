6ef252a515c2b486580f2725b2039af19dbb3114
baumeist <baumeist@adobe.com>
2017-12-07 17:13:56 -0800

    Small randomness

--- analysis/lib/configuration.py
+++ analysis/lib/configuration.py
@@ -39,7 +39,7 @@ class Configuration(object):
         router_drop_rate=[0.0],
         router_loop_detection=['GUIDLoopDetection'],
         #router_randomness=[0.0, 2.0, 4.0],
-        router_randomness=[0.0],
+        router_randomness=[0.0, 2.0],
         routing_data_path=lambda x: os.path.join(
             Configuration.file_path_for_config(x, ''), ROUTING_DATA_FILE_NAME),
         graph_data_path=lambda x: os.path.join(
