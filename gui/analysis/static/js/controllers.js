7ecb0d6e2631876ea8bcf37632eacb6cad7b759d
Todd Baumeister <tbaumeist@gmail.com>
2017-12-08 20:52:29 -0800

    csv table

--- gui/analysis/static/js/controllers.js
+++ gui/analysis/static/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('challengerApp.controllers', [])
-  .controller('ExperimentController', function ($scope, $stateParams, $uibModal, ExperimentService, ExperimentServiceV2, NgTableParams, ExperimentCsvService, ExperimentStaticService, MetricService) {
+  .controller('ExperimentController', function ($scope, $stateParams, $uibModal, ExperimentService, ExperimentServiceV2, ExperimentCsvService, ExperimentStaticService, MetricService) {
     $scope.id = $stateParams.id
     $scope.experiment_v2 = ExperimentServiceV2.get({ id: $stateParams.id })
     $scope.experiment = ExperimentService.get({ id: $stateParams.id })
