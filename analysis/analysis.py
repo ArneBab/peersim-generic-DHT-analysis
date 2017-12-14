3e06c42db2e87b46bd22d5e8c729d82007093c18
Todd Baumeister <tbaumeist@gmail.com>
2017-12-13 17:30:29 -0800

    remove old analysis code

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -11,11 +11,8 @@ import os
 import json
 import multiprocessing
 
-from lib.configuration import ROUTING_DATA_FILE_NAME as R_F_NAME
-from lib.analyzer import Analyzer
-from lib.post_analysis import PostAnalysis
 from lib.utils import timeit
-from lib.summary_metrics import SummaryMetrics
+#from lib.summary_metrics import SummaryMetrics
 
 from lib.metric_manager import MetricManager
 
@@ -40,8 +37,8 @@ class Manager(object):
 
         output_directory = os.path.abspath(args.d)
         total = self.load_experiments(output_directory)
-        self.run_analysis(output_directory, total, args.f, args.t)
-        self.run_summations(output_directory, args.f, args.t)
+        self.run_analysis(total, args.f, args.t)
+        self.run_summations(args.f, args.t)
         logging.info('Finished!!!')
 
     @timeit
@@ -65,7 +62,7 @@ class Manager(object):
         return total
 
     @timeit
-    def run_analysis(self, output_directory, total, must_run, thread_count):
+    def run_analysis(self, total, must_run, thread_count):
         '''
         Run the post run analysis on a experiement
         '''
@@ -81,16 +78,16 @@ class Manager(object):
         for exp_files in self._experiement_configurations:
             if nb_cores > 1:
                 pool.apply_async(_run_analysis, args=(
-                    output_directory, exp_files, count, total, must_run))
+                    exp_files, count, total, must_run))
             else:
-                _run_analysis(output_directory, exp_files,
+                _run_analysis(exp_files,
                               count, total, must_run)
             count += 1
         pool.close()
         pool.join()
 
     @timeit
-    def run_summations(self, output_directory, must_run, thread_count):
+    def run_summations(self, must_run, thread_count):
         '''
         Run after all experiments are complete. Compare the variables
         '''
@@ -112,10 +109,10 @@ class Manager(object):
         for exp_files in exp_grouped.values():
             if nb_cores > 1:
                 groups.append(pool.apply_async(_run_summations, args=(
-                    output_directory, exp_files, count, exp_group_count, must_run)))
+                    exp_files, count, exp_group_count, must_run)))
             else:
-                groups.append(_run_summations(output_directory,
-                                              exp_files, count, exp_group_count, must_run))
+                groups.append(_run_summations(
+                    exp_files, count, exp_group_count, must_run))
             count += 1
         pool.close()
         pool.join()
@@ -125,14 +122,14 @@ class Manager(object):
             holder = [result.get() for result in groups]
             groups = holder
 
-        # caluclate the summary comparision of the experiment runs
-        summary = SummaryMetrics()
-        experiment_data = summary.calculate(groups)
-        with open(os.path.join(output_directory, 'summary.json'), 'w') as s_file:
-            s_file.write(json.dumps(experiment_data))
+        ## caluclate the summary comparision of the experiment runs
+        #summary = SummaryMetrics()
+        #experiment_data = summary.calculate(groups)
+        #with open(os.path.join(output_directory, 'summary.json'), 'w') as s_file:
+        #    s_file.write(json.dumps(experiment_data))
 
-        with open(os.path.join(output_directory, 'summary_display.json'), 'w') as s_file:
-            s_file.write(json.dumps(summary.process(experiment_data)))
+        #with open(os.path.join(output_directory, 'summary_display.json'), 'w') as s_file:
+        #    s_file.write(json.dumps(summary.process(experiment_data)))
 
     def _get_experiments_by_group(self, experiments):
         by_groups = {}
@@ -163,175 +160,31 @@ def _metrics(base_path, *args):
     return path
 
 
-def _run_summations(base_directory, exp_files, count, total, must_run):
+def _run_summations(exp_files, count, total, must_run):
     # base directory
     logging.info('Averaging group %d of %d', count, total)
     count += 1
 
     first_exp = exp_files[0]
     base_path = _get_base(_get_base(first_exp[CONST_CONFIG]))
-    output_file = _metrics(base_path, 'consolidated.json')
-
-    # START NEW CODE
 
     metric_manager = MetricManager(base_path, must_run)
     metric_manager.summarize()
     metric_manager.save_data()
-
-    # END NEW CODE
-
-    # check if we can skip running the summation
-    if not must_run and os.path.exists(output_file):
-        logging.info('Already analyzed group ... skipping')
-        return output_file
-
-    conf_files = [exp[CONST_CONFIG] for exp in exp_files]
-
-    analyzer = Analyzer(conf_files)
-    if not os.path.exists(_metrics(base_path)):
-        os.makedirs(_metrics(base_path))
-
-    # routing choice stats
-    with open(_metrics(base_path, 'stats.json'), 'w') as s_file:
-        routing_choice_avg, graph_data = analyzer.run_routing_choice_metrics()
-        s_file.write(json.dumps(graph_data))
-
-    r_metrics = analyzer.get_routing_metrics(base_directory, None, None, None)
-    r_metrics.calculate_metrics()
-    _write_analysis_data(base_path, r_metrics)
-    return output_file
+    return metric_manager.metric_file_path
 
 
-def _run_analysis(output_directory, exp_files, count, total, must_run):
-    _run_pre_analysis(output_directory, exp_files, count, total, must_run)
-    _run_post_analysis(exp_files, count, total, must_run)
-
-
-def _run_pre_analysis(base_directory, exp_files, count, total, must_run):
-    logging.info('Running pre analysis %d of %d', count, total)
+def _run_analysis(exp_files, count, total, must_run):
+    logging.info('Running analysis %d of %d', count, total)
 
     # base directory
     base_path = _get_base(exp_files[CONST_CONFIG])
-
-    # START NEW CODE
 
     # calculate analysis metrics
     metric_manager = MetricManager(base_path, must_run)
     metric_manager.analyze()
     metric_manager.save_data()
     metric_manager.archive_data()
-
-    # END NEW CODE
-
-    # skip analysis if it alreay done
-    if not must_run and os.path.exists(_metrics(base_path, 'intercept_calculated.json')):
-        logging.info('Already pre analyzed ... skipping')
-        return
-
-    analyzer = Analyzer([exp_files[CONST_CONFIG]])
-    if not os.path.exists(_metrics(base_path)):
-        os.makedirs(_metrics(base_path))
-
-    # routing choice stats
-    with open(_metrics(base_path, 'stats.json'), 'w') as s_file:
-        routing_choice_avg, graph_data = analyzer.run_routing_choice_metrics()
-        s_file.write(json.dumps(graph_data))
-
-    # routing metrics
-    routing_data_name = _base(base_path, R_F_NAME)
-    new_routing_data = _base(base_path, 'processed.' + R_F_NAME)
-
-    r_metrics = analyzer.get_routing_metrics(base_directory,
-                                             routing_data_name, new_routing_data, routing_choice_avg)
-    r_metrics.calculate_metrics()
-    _write_analysis_data(base_path, r_metrics)
-
-
-def _run_post_analysis(exp_files, count, total, must_run):
-    logging.info('Running post analysis %d of %d', count, total)
-
-    # base directory
-    base_path = _get_base(exp_files[CONST_CONFIG])
-
-    # skip analysis if it alreay done
-    if not must_run and os.path.exists(_metrics(base_path, 'anonymity.csv')):
-        logging.info('Already post analyzed ... skipping')
-        return
-
-    analyzer = PostAnalysis(base_path)
-    if not os.path.exists(_metrics(base_path)):
-        os.makedirs(_metrics(base_path))
-
-    # write performance data
-    with(open(_metrics(base_path, 'performance.csv'), 'w')) as p_file:
-        for row in analyzer.convert_csv_performance():
-            p_file.write(','.join(row))
-            p_file.write('\n')
-
-    # write anonymity data to csv
-    with(open(_metrics(base_path, 'anonymity.csv'), 'w')) as p_file:
-        for row in analyzer.convert_csv_anonymity():
-            p_file.write(','.join(row))
-            p_file.write('\n')
-
-    #analyzer.process_performance(_metrics(base_path, 'performance.csv'))
-    # not needed at this point
-    #analyzer.process_anonymity(_metrics(base_path, 'anonymity.csv'))
-
-
-def _write_analysis_data(base_path, r_metrics):
-    # path length
-    with open(_metrics(base_path, 'path_histo.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_path_lengths()))
-    # anon metrics
-    with open(_metrics(base_path, 'sender_set_size.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_sender_set()))
-
-    with open(_metrics(base_path, 'sender_set_size_by_hop.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_sender_set_by_hop()))
-    # entropy metrics
-    with open(_metrics(base_path, 'entropy.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_entropy()))
-    with open(_metrics(base_path, 'entropy_normalized.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_entropy_normalized()))
-
-    with open(_metrics(base_path, 'entropy_by_hop.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_entropy_by_hop()))
-    with open(_metrics(base_path, 'entropy_normalized_by_hop.json'), 'w') as g_file:
-        g_file.write(json.dumps(
-            r_metrics.graph_entropy_normalized_by_hop()))
-
-    with open(_metrics(base_path, 'top_rank_set_size_by_hop.json'), 'w') as g_file:
-        g_file.write(json.dumps(
-            r_metrics.graph_top_rank_set_size_by_hop()))
-
-    with open(_metrics(base_path, 'top_rank_by_hop.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_top_rank_by_hop()))
-    # entropy metric using actual backoff
-    with open(_metrics(base_path, 'entropy_actual.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_entropy_actual()))
-    with open(_metrics(base_path, 'entropy_normalized_actual.json'), 'w') as g_file:
-        g_file.write(json.dumps(
-            r_metrics.graph_entropy_normalized_actual()))
-
-    with open(_metrics(base_path, 'entropy_by_hop_actual.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_entropy_by_hop_actual()))
-    with open(_metrics(base_path, 'entropy_normalized_by_hop_actual.json'), 'w') as g_file:
-        g_file.write(json.dumps(
-            r_metrics.graph_entropy_normalized_by_hop_actual()))
-
-    # consolidated metrics
-    with open(_metrics(base_path, 'consolidated.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.get_summary()))
-    # intercept hop
-    with open(_metrics(base_path, 'intercept.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_intercept_hop()))
-    with open(_metrics(base_path, 'intercept_percent.json'), 'w') as g_file:
-        g_file.write(json.dumps(r_metrics.graph_intercept_percent_hop()))
-    # intercept hop
-    with open(_metrics(base_path, 'intercept_calculated.json'), 'w') as g_file:
-        g_file.write(json.dumps(
-            r_metrics.graph_intercept_hop_calculated()))
 
 
 if __name__ == '__main__':
