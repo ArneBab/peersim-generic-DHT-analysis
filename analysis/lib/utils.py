bc86e8629b56c0e24323c9541124e68d29353d15
baumeist <baumeist@adobe.com>
2017-12-17 11:50:39 -0800

    code clean up

--- analysis/lib/utils.py
+++ analysis/lib/utils.py
@@ -20,61 +20,6 @@ def average_degree(nx_graph):
     return sum(dict(nx_graph.degree()).values()) / float(nx_graph.number_of_nodes())
 
 
-def to_histogram_ints(values, start=None, stop=None, bin_funct=None):
-    '''
-    Convert a list of numbers into a histogram
-    :param values: List of numbers
-    :param start: The start value for the histogram
-    :param stop: The stop value for the histogram
-    :param bin_funct: When there are not natural bin sizes, they can be defined with this function
-    :return: Labels list, data list, start value, stop value
-    '''
-    holder, start, stop = _to_histogram_(values, start, stop, bin_funct)
-    histo = []
-    labels = []
-    for i in range(start, stop + 1):
-        labels.append(i)
-        if i in holder:
-            histo.append(holder[i])
-        else:
-            histo.append(0)
-    return labels, histo, start, stop
-
-def to_histogram_floats(values, start=None, stop=None, bin_funct=None, labels_funct=None):
-    '''
-    Convert a list of numbers into a histogram
-    :param values: List of numbers
-    :param start: The start value for the histogram
-    :param stop: The stop value for the histogram
-    :param bin_funct: When there are not natural bin sizes, they can be defined with this function
-    :param label_funct: Define the label values (same as the bin_funct)
-    :return: Labels list, data list, start value, stop value
-    '''
-    holder, start, stop = _to_histogram_(values, start, stop, bin_funct)
-    histo = []
-    labels = []
-    for i in labels_funct():
-        labels.append(i)
-        if i in holder:
-            histo.append(holder[i])
-        else:
-            histo.append(0)
-    return labels, histo, start, stop
-
-def frange(start, stop, jump, places):
-    '''
-    Range for floats
-    :param start: start
-    :param stop: stop
-    :param jump: step
-    :param places: decimal places of percision
-    :return: list of floats
-    '''
-    while start < stop:
-        yield start
-        start = round(start + jump, places)
-
-
 def distance(x_l, y_l):
     '''
     Calculate the distance between x and y with a sircular address space
@@ -133,37 +78,6 @@ def percent(selected, total):
         return 0.0
     return selected / float(total)
 
-def _to_histogram_(values, start=None, stop=None, bin_funct=None):
-    if len(values) <= 0:
-        return None, None, None
-
-    def _natural(value): return value
-
-    if bin_funct is None:
-        bin_funct = _natural
-
-    holder = {}
-    largest = None
-    smallest = None
-    for value in values:
-        bucket = bin_funct(value)
-        # init entry
-        if bucket not in holder:
-            holder[bucket] = 0
-        holder[bucket] = holder[bucket] + 1
-        if largest is None:
-            largest = bucket
-        if smallest is None:
-            smallest = bucket
-        if bucket > largest:
-            largest = bucket
-        if bucket < smallest:
-            smallest = bucket
-    if start is None:
-        start = smallest
-    if stop is None:
-        stop = largest
-    return holder, start, stop
 
 def timeit(method):
     ''' Logs a method call time '''
