18516badd7fa27d263be775f67ce644a17346e3e
Todd Baumeister <tbaumeist@gmail.com>
2018-01-22 22:24:25 -0800

    Read routing paths from zip file

--- gui/analysis/resources/api/data.py
+++ gui/analysis/resources/api/data.py
@@ -7,6 +7,7 @@ API for getting experiment metrics
 '''
 import os
 import json
+import zipfile
 import jmespath
 from flask_restful import Resource
 from flask import jsonify, current_app, request
@@ -21,23 +22,28 @@ class Data(Resource):
         '''
         Only supports get operation
         '''
-        if 'filter' not in request.args or not request.args.get('filter'):
+        if 'filter' not in request.args:
             return jsonify({'items': []})
 
         experiment_config = current_app.config['EXPERIMENT_LIST'][experiment_id]
 
-        routes_path = os.path.join(os.path.dirname(
-            experiment_config['self']), 'processed.routing.json')
-        expression = jmespath.compile(request.args.get('filter'))
+        archive_path = os.path.abspath(os.path.join(os.path.dirname(
+            experiment_config['self']), 'archive.zip'))
+
+        expression = None
+        if request.args.get('filter'):
+            expression = jmespath.compile(request.args.get('filter'))
         routes = []
-        if os.path.exists(routes_path):
+        if os.path.exists(archive_path):
             count = 0
-            with open(routes_path, 'r') as c_file:
-                for line in c_file:
-                    r_json = json.loads(line)
-                    if expression.search(r_json):
-                        routes.append(r_json)
-                        count += 1
-                        if count >= 1000:
-                            break
+            with zipfile.ZipFile(archive_path, allowZip64=True) as z_file:
+                if 'sender_set.routing.json' in z_file.namelist():
+                    with z_file.open('sender_set.routing.json') as c_file:
+                        for line in c_file:
+                            r_json = json.loads(line)
+                            if expression is None or expression.search(r_json):
+                                routes.append(r_json)
+                                count += 1
+                                if count >= 100:
+                                    break
         return jsonify({'items': routes})
