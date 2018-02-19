36e9bdbbefb18826efa8d420442fe7cde77f0ce0
baumeist <baumeist@adobe.com>
2018-02-18 19:30:42 -0800

    read route data from archive and plain file

--- gui/analysis/resources/api/data.py
+++ gui/analysis/resources/api/data.py
@@ -29,21 +29,34 @@ class Data(Resource):
 
         archive_path = os.path.abspath(os.path.join(os.path.dirname(
             experiment_config['self']), 'archive.zip'))
+        normal_path = os.path.abspath(os.path.join(os.path.dirname(
+            experiment_config['self']), 'sender_set.routing.json'))
 
         expression = None
         if request.args.get('filter'):
             expression = jmespath.compile(request.args.get('filter'))
         routes = []
-        if os.path.exists(archive_path):
-            count = 0
+
+        if os.path.exists(normal_path):
+            # read from normal file
+            with open(normal_path, 'r') as c_file:
+                self._filter(c_file, expression, routes)
+        elif os.path.exists(archive_path):
+            # read from zip file
             with zipfile.ZipFile(archive_path, allowZip64=True) as z_file:
                 if 'sender_set.routing.json' in z_file.namelist():
                     with z_file.open('sender_set.routing.json') as c_file:
-                        for line in c_file:
-                            r_json = json.loads(line)
-                            if expression is None or expression.search(r_json):
-                                routes.append(r_json)
-                                count += 1
-                                if count >= 100:
-                                    break
+                        self._filter(c_file, expression, routes)
+
         return jsonify({'items': routes})
+
+    def _filter(self, c_file, expression, routes):
+        count = 0
+        for line in c_file:
+            r_json = json.loads(line)
+            if expression is None or expression.search(r_json):
+                routes.append(r_json)
+                count += 1
+                if count >= 100:
+                    break
+        
